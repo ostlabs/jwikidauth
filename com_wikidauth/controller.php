19f6216284a236c60207e1eee5ceb317dbbb0376
coolacid <coolacid@3e8cfaed-0f6e-4f5c-a16f-a6e8fc484f5f>
2010-08-11 01:58:27 +0000

    Initial work on the option component

---
+++ com_wikidauth/controller.php
@@ -0,0 +1,62 @@
+<?php
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die( 'Restricted access' );
+
+jimport( 'joomla.application.component.controller' );
+
+class WikidAuthController extends JController
+{
+    // Register Token
+    function display()
+    {
+	?>
+	<script language="javascript" type="text/javascript">                                                                                                                                                                                                      
+	<!--                                                                                                                                                                                                                                                       
+            function submitbutton(pressbutton) {                                                                                                                                                                                                               
+	        var form = document.usereditForm;                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                           
+                // do field validation                                                                                                                                                                                                                         
+        	form.submit();                                                                                                                                                                                                                                 
+    	    }                                                                                                                                                                                                                                                  
+	-->                                                                                                                                                                                                                                                        
+	</script>
+	<?
+
+	echo "Please enter the registration code provided when adding a domain on your soft token.<BR /> <BR />";
+	echo "<form action='" . JURI::base() . "index.php' name='usereditForm' method='post'>";
+	echo "Registration code: <input type='text' name='regcode'><BR />";
+	echo "<input type='hidden' name='option' value='com_wikidauth' />";
+	echo "<input type='hidden' name='task' value='register_submit' />";
+	//token
+	echo JHTML::_( 'form.token' );
+        echo "<button class='button' onclick='return submitbutton(\'send\');'>";
+	echo "	" . JText::_('SEND');
+        echo "</button>";
+	echo "<BR /><BR /><BR /><BR />";
+    }
+
+    function register_submit()
+    {
+	$username = "coolacid";
+
+	// Make sure we actually have a register code.
+	$regcode = JRequest::getVar('regcode', null, 'post', 'cmd');
+
+	// Acctually register the token by calling the register function in plugin.
+	$plugin = JPluginHelper::getPlugin('authentication', 'wikidauth');
+	JPluginHelper::importPlugin('authentication', 'wikidauth');
+
+	$dispatcher = & JDispatcher::getInstance();
+
+	$className = 'plg'.$plugin->type.$plugin->name;
+	$wikidauth = new $className($dispatcher, (array)$plugin);
+
+	$result = $wikidauth->register($username, $regcode);
+
+	if ( $result == 0 ) {
+	    echo "Token registered successfully.<br /><br />";
+	} else {
+	    echo "There was an error registering your token: $result<br /><br />";
+	}
+    }
+}
